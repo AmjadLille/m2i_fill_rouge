db34ca01428d31e462b9c73596a308e76b19f49e
Xylaise <tr.m.heutte@gmail.com>
2021-05-10 17:52:02 +0200

    tentative de merge

--- ProjetFilRouge/ProjetFilRouge/BDD/Request.sql
+++ ProjetFilRouge/ProjetFilRouge/BDD/Request.sql
@@ -1,7 +1,7 @@
-﻿--Select * from users;
+﻿Select * from users;
 --select * from canal;
 --select * from Commentaire;
-select * from UserInCanal;
+--select * from UserInCanal;
 
 -- Select * users avec affichage des statuts
 --Select nom, prenom, pseudo, mdp, email, 
@@ -9,17 +9,22 @@ select * from UserInCanal;
 --REPLACE(isStatut, 2, 'Actif')
 --from Users
 
--- Récupére le theme du canal avec le pseudo du 
+---- Récupére le theme du canal avec le pseudo du 
 -- créateur
 --Select Canal.theme, Users.pseudo as Owner
 --From Canal
 --join Users
 --on Canal.idUser = Users.id
 
-Select Canal.theme, Users.pseudo
-from UserInCanal
-join Users
-on UserInCanal.idUser = Users.id 
-join Canal
-on UserInCanal.idCanal = Canal.id 
+---- Récupérer le theme et le pseudo 
+--Select Canal.theme, Users.pseudo
+--from UserInCanal
+--join Users
+--on UserInCanal.idUser = Users.id 
+--join Canal
+--on UserInCanal.idCanal = Canal.id 
+
+---- Modification du statut de l'inscription
+--UPDATE Users set isStatut = 1
+--where isStatut = 3 and Users.id = 1;
 
