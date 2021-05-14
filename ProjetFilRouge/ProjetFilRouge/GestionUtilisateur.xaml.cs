c98e9e3297f117cc68ffd5a8c3446a144196ab8e
yohan <kassiryo@gmail.com>
2021-05-14 15:30:38 +0200

    GestonUtilisateur

--- ProjetFilRouge/ProjetFilRouge/GestionUtilisateur.xaml.cs
+++ ProjetFilRouge/ProjetFilRouge/GestionUtilisateur.xaml.cs
@@ -23,7 +23,42 @@ namespace ProjetFilRouge
         }
 
         private void AjouterUtilisateur_Click(object sender, RoutedEventArgs e)
-        { }
+        {
+            if (NomUtilisateur.Text != "")
+            {
+                if (PrenomUtilisateur.Text != "" || EmailUtilisateur.Text != "" || PseudoUtilisateur.Text != "" || MdpUtilisateur.Text != "")
+                {
+                    MessageBoxResult result = MessageBox.Show("Merci de confirmer l'ajout : " + NomUtilisateur.Text, "Confirmer", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                    switch (result)
+                    {
+                        case MessageBoxResult.Yes:
+                            Utilisateur u = new Utilisateur(0, NomUtilisateur.Text, PrenomUtilisateur.Text, PseudoUtilisateur.Text, MdpUtilisateur.Text, EmailUtilisateur.Text, 2,false );
+                            if (Contenu.AjouterContenu(c) != -1)
+                            { MessageBox.Show("Utilisateur ajouté", "Ajout fait", MessageBoxButton.OK, MessageBoxImage.Information); }
+                            break;
+                        case MessageBoxResult.No:
+                            Close();
+                            break;
+                        default:
+                            break;
+                    }
+                    NomUtilisateur.Clear();
+                    PrenomUtilisateur.Clear();
+                    EmailUtilisateur.Clear();
+                    PseudoUtilisateur.Clear();
+                    MdpUtilisateur.Clear();
+
+                    StatutActif.IsChecked = false;
+                    StatutInactif.IsChecked = false;
+                    AdminOui.IsChecked = false;
+                    AdminNon.IsChecked = false;
+                }
+                else
+                { MessageBox.Show("Aucun Utilisateur ajouté", "Utilisateur non ajouté", MessageBoxButton.OK, MessageBoxImage.Error); }
+            }
+            else
+            { MessageBox.Show("Vous devez donner un nom à votre Utilisateur", "Erreur titre", MessageBoxButton.OK, MessageBoxImage.Error); }
+        }
 
         private void ModifierUtilisateur_Click(object sender, RoutedEventArgs e)
         { }
